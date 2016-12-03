a4f3df6b2491809eb627271a1c9362c3a42bcac1
herlihke <herlihke@pascack.k12.nj.us>
2016-12-03 09:58:38 -0500

    Add files via upload
    Point method

---
+++ Point.java
@@ -0,0 +1,31 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+/*
+Name: Kevin Herlihy
+Date: Dec 3, 2016
+Program Name: Point.java
+Description:
+*/
+
+public class Point
+{
+	private double x;
+	private double y;
+	public static final int radius = 1; //sets radius
+	
+	public Point(double xCoord, double yCoord) //creates point
+	{
+		x = xCoord;
+		y = yCoord;
+		
+	}
+	public void renderPoint(Graphics g) //draws point on the grid
+	{
+		//super.paintComponent(g);
+      Graphics2D g2 = (Graphics2D) g;
+      
+		g2.drawOval((int)x, (int)y, radius, radius); //convert x & y coords to ints in order to draw circle
+	}
+	
+}
